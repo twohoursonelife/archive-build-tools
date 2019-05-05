c5b7b6e456b01fee9c36f7429b8557d898993c04
Richard Thomas <ri@chrd.uk>
2019-05-05 15:48:46 +0100

    Add macOS builds to scripts

--- scripts/BuildAndPostClients.sh
+++ scripts/BuildAndPostClients.sh
@@ -15,8 +15,9 @@ cd repos;
 
 
 ###############################################################################
-## Versions ###################################################################
+## Preparation ################################################################
 ###############################################################################
+
 
 # Determine the current and previous versions.
 
@@ -24,9 +25,28 @@ PREVIOUS_VERSION="$(git -C "${REPO_CODE}" for-each-ref --sort=-creatordate \
     --format="%(refname:short)" --count=2 "refs/tags/${TAG_BASE}*" | \
     tail -n 1 | sed "s/^${TAG_BASE}//")";
 
+PREVIOUS_VERSION_REF="${TAG_BASE}${PREVIOUS_VERSION}";
+
 CURRENT_VERSION="$(git -C "${REPO_CODE}" for-each-ref --sort=-creatordate \
     --format="%(refname:short)" --count=1 "refs/tags/${TAG_BASE}*" | \
     sed "s/^${TAG_BASE}//")";
+
+CURRENT_VERSION_REF="${TAG_BASE}${CURRENT_VERSION}";
+
+
+# Override versions when asked to build from master.
+
+if [[ -n "${CRUCIBLE_BUILD_MASTER}" ]]; then
+
+    PREVIOUS_VERSION="${CURRENT_VERSION}";
+
+    PREVIOUS_VERSION_REF="${CURRENT_VERSION_REF}";
+
+    CURRENT_VERSION="${CURRENT_VERSION}+$(date "+%Y%m%dT%H%M%S")";
+
+    CURRENT_VERSION_REF="master";
+
+fi;
 
     
 
@@ -34,16 +54,18 @@ CURRENT_VERSION="$(git -C "${REPO_CODE}" for-each-ref --sort=-creatordate \
 ## Linux ######################################################################
 ###############################################################################
 
+
 # Clean the repositories.
 
 for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
     git -C "${REPO}" clean -f -x -d;
 done;
 
+
 # Build the current version distribution.
 
 for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
-    git -C "${REPO}" checkout "${TAG_BASE}${CURRENT_VERSION}";
+    git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
 done;
 
 cd OneLife;
@@ -62,12 +84,13 @@ cp "OneLife_v${CURRENT_VERSION}_Linux.tar.gz" ~;
 
 cd ../../..;
 
+
 # Build the previous version distribution.
 
 if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
     for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
-        git -C "${REPO}" checkout "${TAG_BASE}${PREVIOUS_VERSION}";
+        git -C "${REPO}" checkout "${PREVIOUS_VERSION_REF}";
     done;
 
     cd OneLife;
@@ -86,12 +109,13 @@ if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
 fi;
 
+
 # Build the incremental bundle.
 
 if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
     for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
-        git -C "${REPO}" checkout "${TAG_BASE}${CURRENT_VERSION}";
+        git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
     done;
 
     cd minorGems/game/diffBundle;
@@ -111,11 +135,12 @@ if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
 fi;
 
+
 # Clean everything up.
 
 for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
     git -C "${REPO}" clean -f -x -d;
-    git -C "${REPO}" checkout "${TAG_BASE}${CURRENT_VERSION}";
+    git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
 done;
 
 
@@ -124,15 +149,23 @@ done;
 ## Windows ####################################################################
 ###############################################################################
 
+
 # Clean the repositories.
 
 for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
     git -C "${REPO}" clean -f -x -d;
 done;
 
+
 # Prepare the foreign SDL library.
 
 cd ..;
+
+if [[ ! -d windows_deps ]]; then
+    mkdir windows_deps;
+fi;
+
+cd windows_deps;
 
 if [[ ! -f SDL-devel-1.2.15-mingw32.tar.gz ]]; then
     wget https://www.libsdl.org/release/SDL-devel-1.2.15-mingw32.tar.gz;
@@ -142,16 +175,17 @@ if [[ ! -d SDL-1.2.15 ]]; then
     tar -xzf SDL-devel-1.2.15-mingw32.tar.gz;
 fi;
 
+ln -s "$(realpath .)/SDL-1.2.15/include/SDL" ../repos/SDL;
+
 export CUSTOM_MINGW_LINK_FLAGS="-L$(realpath .)/SDL-1.2.15/lib";
 
-cd repos;
+cd ../repos;
 
-ln -s ../SDL-1.2.15/include/SDL SDL;
 
 # Build the current version distribution.
 
 for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
-    git -C "${REPO}" checkout "${TAG_BASE}${CURRENT_VERSION}";
+    git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
 done;
 
 cd OneLife;
@@ -180,12 +214,13 @@ cp "OneLife_v${CURRENT_VERSION}_Windows.zip" ~;
 
 cd ../../..;
 
+
 # Build the previous version distribution.
 
 if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
     for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
-        git -C "${REPO}" checkout "${TAG_BASE}${PREVIOUS_VERSION}";
+        git -C "${REPO}" checkout "${PREVIOUS_VERSION_REF}";
     done;
 
     cd OneLife;
@@ -214,12 +249,13 @@ if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
 fi;
 
+
 # Build the incremental bundle.
 
 if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
     for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
-        git -C "${REPO}" checkout "${TAG_BASE}${CURRENT_VERSION}";
+        git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
     done;
 
     cd minorGems/game/diffBundle;
@@ -239,6 +275,7 @@ if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
 
 fi;
 
+
 # Clean everything up.
 
 unset CUSTOM_MINGW_LINK_FLAGS;
@@ -247,5 +284,167 @@ rm SDL;
 
 for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
     git -C "${REPO}" clean -f -x -d;
-    git -C "${REPO}" checkout "${TAG_BASE}${CURRENT_VERSION}";
+    git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
+done;
+
+
+
+###############################################################################
+## macOS ######################################################################
+###############################################################################
+
+
+# Clean the repositories.
+
+for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
+    git -C "${REPO}" clean -f -x -d;
+done;
+
+
+# Prepare the foreign SDL library.
+
+cd ..;
+
+if [[ ! -d macos_deps ]]; then
+    mkdir macos_deps;
+fi;
+
+cd macos_deps;
+
+if [[ ! -f SDL-1.2.15.dmg ]]; then
+    wget https://www.libsdl.org/release/SDL-1.2.15.dmg;
+fi;
+
+if [[ ! -f SDL-1.2.15.img ]]; then
+    dmg2img SDL-1.2.15.dmg;
+fi;
+
+if [[ ! -d SDL.framework ]]; then
+
+    mkdir sdl_mount;
+
+    sudo mount -o loop -t hfsplus SDL-1.2.15.img sdl_mount;
+
+    cp -R sdl_mount/SDL.framework .;
+
+    sudo umount sdl_mount;
+
+    rm -r sdl_mount;
+
+fi;
+
+export CUSTOM_MACOSX_LINK_FLAGS="-F$(realpath .)";
+
+cd ../repos;
+
+
+# Build the current version distribution.
+
+for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
+    git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
+done;
+
+cd OneLife;
+
+./configure 2;
+
+cd gameSource;
+
+_OLD_PATH="${PATH}"; export PATH="/home/richard/macos/osxcross/target/bin:${PATH}";
+
+cp Makefile Makefile.bak;
+
+sed -i -r '/^GXX =/aGXX = o32-g++' Makefile;
+
+sed -i -r '/PLATFORM_COMPILE_FLAGS =/aPLATFORM_COMPILE_FLAGS += ${CUSTOM_MACOSX_LINK_FLAGS}' Makefile;
+
+make;
+
+mv Makefile.bak Makefile;
+
+export PATH="${_OLD_PATH}"; unset _OLD_PATH;
+
+cd ../build;
+
+./makeDistributionMacOSX "v${CURRENT_VERSION}" IntelMacOSX ../../../macos_deps/SDL.framework;
+
+cd mac;
+
+cp "OneLife_v${CURRENT_VERSION}_IntelMacOSX.tar.gz" ~;
+
+cd ../../..;
+
+
+# Build the previous version distribution.
+
+if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
+
+    for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
+        git -C "${REPO}" checkout "${PREVIOUS_VERSION_REF}";
+    done;
+
+    cd OneLife;
+
+    ./configure 2;
+
+    cd gameSource;
+
+    _OLD_PATH="${PATH}"; export PATH="/home/richard/macos/osxcross/target/bin:${PATH}";
+
+    cp Makefile Makefile.bak;
+
+    sed -i -r '/^GXX =/aGXX = o32-g++' Makefile;
+
+    sed -i -r '/PLATFORM_COMPILE_FLAGS =/aPLATFORM_COMPILE_FLAGS += ${CUSTOM_MACOSX_LINK_FLAGS}' Makefile;
+
+    make;
+
+    mv Makefile.bak Makefile;
+
+    export PATH="${_OLD_PATH}"; unset _OLD_PATH;
+
+    cd ../build;
+
+    ./makeDistributionMacOSX "v${PREVIOUS_VERSION}" IntelMacOSX ../../../macos_deps/SDL.framework;
+
+    cd mac;
+
+    cp "OneLife_v${PREVIOUS_VERSION}_IntelMacOSX.tar.gz" ~;
+
+    cd ../../..;
+
+fi;
+
+
+# Build the incremental bundle.
+
+if [[ "${CURRENT_VERSION}" -ge 3 ]]; then
+
+    for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
+        git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
+    done;
+
+    cd minorGems/game/diffBundle;
+
+    ./diffBundleCompile;
+
+    cd ../../../OneLife/build/mac;
+
+    ../../../minorGems/game/diffBundle/diffBundle \
+        "OneLife_v${PREVIOUS_VERSION}" \
+        "OneLife_v${CURRENT_VERSION}" \
+        "${CURRENT_VERSION}_inc_mac.dbz";
+
+    cp "${CURRENT_VERSION}_inc_mac.dbz" ~;
+
+    cd ../../..;
+
+fi;
+
+
+# Clean everything up.
+
+for REPO in ${REPO_CODE} ${REPO_DATA} ${REPO_GEMS}; do
+    git -C "${REPO}" clean -f -x -d;
+    git -C "${REPO}" checkout "${CURRENT_VERSION_REF}";
 done;
