0843470b7cb18a3725583e6e5b385b033d6d322e
connorhsm <connorhsmith@gmail.com>
2020-04-01 15:58:56 +1100

    Converted PrepareRepositories.sh to 2HOL

--- scripts/PrepareRepositories.sh
+++ scripts/PrepareRepositories.sh
@@ -9,33 +9,28 @@ source ./scripts/CommonVariables.sh;
 
 
 GITHUB_USER="${1}";
+GITHUB_BASE="https://github.com/twohoursonelife";
 
-if [[ -z "${GITHUB_USER}" ]]; then
-    GITHUB_BASE="https://github.com/chardbury";
-else
-    GITHUB_BASE="git@github.com:${GITHUB_USER}";
-fi;
-
-if [[ ! -d repos/crucible-code ]]; then
-    git clone "${GITHUB_BASE}/crucible-code.git" repos/crucible-code;
+if [[ ! -d repos/2HOL-code ]]; then
+    git clone "${GITHUB_BASE}/OneLife.git" repos/2HOL-code;
 fi;
 
 if [[ ! -e repos/OneLife ]]; then
-    ln -s crucible-code repos/OneLife;
+    ln -s 2HOL-code repos/OneLife;
 fi;
 
-if [[ ! -d repos/crucible-data ]]; then
-    git clone "${GITHUB_BASE}/crucible-data.git" repos/crucible-data;
+if [[ ! -d repos/2HOL-data ]]; then
+    git clone "${GITHUB_BASE}/OneLifeData7.git" repos/2HOL-data;
 fi;
 
 if [[ ! -e repos/OneLifeData7 ]]; then
-    ln -s crucible-data repos/OneLifeData7;
+    ln -s 2HOL-data repos/OneLifeData7;
 fi;
 
-if [[ ! -e repos/crucible-gems ]]; then
-    git clone "${GITHUB_BASE}/crucible-gems.git" repos/crucible-gems;
+if [[ ! -e repos/2HOL-gems ]]; then
+    git clone "${GITHUB_BASE}/minorGems.git" repos/2HOL-gems;
 fi;
 
 if [[ ! -e repos/minorGems ]]; then
-    ln -s crucible-gems repos/minorGems;
+    ln -s 2HOL-gems repos/minorGems;
 fi;
